8eef10c3ea2029a3792844bf13291327079786ff
LiYan <332473716@qq.com>
2022-12-30 16:58:09 +0800

    项目初始化

---
+++ src/main/java/com/brucelee9527/ssmvuedemo/SsmvuedemoApplication.java
@@ -0,0 +1,13 @@
+package com.brucelee9527.ssmvuedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmvuedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmvuedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brucelee9527/ssmvuedemo/SsmvuedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brucelee9527.ssmvuedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmvuedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
